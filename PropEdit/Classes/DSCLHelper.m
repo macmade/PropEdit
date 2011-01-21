ae77acf6248126d26e2c01996c364b05b521d18f
macmade <macmade>
2011-01-21 15:49:20 +0000

    Added a helper class for DSCL.

---
+++ PropEdit/Classes/DSCLHelper.m
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2011, Jean-David Gadina <macmade@eosgarden.com>
+ * All rights reserved
+ ******************************************************************************/
+ 
+/* $Id$ */
+
+/*!
+ * @header      DSCLHelper.m
+ * @copyright   eosgarden 2011 - Jean-David Gadina <macmade@eosgarden.com>
+ * @abstract    ...
+ */
+
+#import "DSCLHelper.h"
+
+@implementation DSCLHelper
+
+
+
+@end
---
+++ PropEdit/Classes/include/DSCLHelper.h
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2011, Jean-David Gadina <macmade@eosgarden.com>
+ * All rights reserved
+ ******************************************************************************/
+ 
+/* $Id$ */
+
+/*!
+ * @header      DSCLHelper.h
+ * @copyright   eosgarden 2011 - Jean-David Gadina <macmade@eosgarden.com>
+ * @abstract    ...
+ */
+
+@interface DSCLHelper: NSObject
+{
+@protected
+    
+    
+    
+@private
+   
+   id r1;
+   id r2;
+}
+
+@end
